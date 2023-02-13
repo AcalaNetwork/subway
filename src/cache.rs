125e853c4562dc20e5af9f9869413ded3cf318eb
Bryan Chen <xlchen1291@gmail.com>
2023-02-13 15:55:43 +1300

    refactor cache and add tests

--- src/cache.rs
+++ src/cache.rs
@@ -1,28 +1,57 @@
+use blake2::digest::Output;
+use blake2::Digest;
 use jsonrpsee::core::JsonValue;
 use lru::LruCache;
 use std::num::NonZeroUsize;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
-pub struct Cache {
-    map: Arc<Mutex<LruCache<[u8; 64], JsonValue>>>,
+#[derive(Clone, Debug)]
+pub struct CacheKey<D: Digest>(pub Output<D>);
+
+impl<D: Digest> CacheKey<D> {
+    pub fn new(method: &String, params: &Vec<JsonValue>) -> Self {
+        let mut hasher = D::new();
+        hasher.update(method.as_bytes());
+        for p in params {
+            hasher.update(p.to_string().as_bytes());
+        }
+
+        Self(hasher.finalize())
+    }
 }
 
-impl Cache {
+impl<D: Digest> PartialEq for CacheKey<D> {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.as_slice() == other.0.as_slice()
+    }
+}
+
+impl<D: Digest> Eq for CacheKey<D> {}
+
+impl<D: Digest> std::hash::Hash for CacheKey<D> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.0.as_slice().hash(state);
+    }
+}
+
+#[derive(Clone)]
+pub struct Cache<D: Digest> {
+    map: Arc<Mutex<LruCache<CacheKey<D>, JsonValue>>>,
+}
+
+impl<D: Digest + 'static> Cache<D> {
     pub fn new(size: NonZeroUsize) -> Self {
         Self {
             map: Arc::new(Mutex::new(LruCache::new(size))),
         }
     }
 
-    pub async fn get(&self, key: &[u8; 64]) -> Option<JsonValue> {
+    pub async fn get(&self, key: &CacheKey<D>) -> Option<JsonValue> {
         self.map.lock().await.get(key).cloned()
     }
 
-    pub fn put(&self, key: [u8; 64], value: JsonValue) {
-        let map = self.map.clone();
-        tokio::spawn(async move {
-            let _ = map.lock().await.put(key, value);
-        });
+    pub async fn put(&self, key: CacheKey<D>, value: JsonValue) {
+        self.map.lock().await.put(key, value);
     }
 }
