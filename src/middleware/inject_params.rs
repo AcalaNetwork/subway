873629fc763c721146d32e342ef1ca8bafa295e3
Shaun Wang <spxwang@gmail.com>
2023-02-23 15:49:51 +1300

    unit tests

--- src/middleware/inject_params.rs
+++ src/middleware/inject_params.rs
@@ -122,3 +122,121 @@ impl Middleware<CallRequest, Result<JsonValue, Error>> for InjectParamsMiddlewar
         }
     }
 }
+
+mod tests {
+    use super::*;
+
+    use crate::client::{mock::TestServerBuilder, Client};
+    use futures::FutureExt;
+    use jsonrpsee::{server::ServerHandle, SubscriptionMessage, SubscriptionSink};
+    use serde_json::json;
+    use tokio::sync::{mpsc, oneshot};
+
+    async fn create_client() -> (
+        Api,
+        ServerHandle,
+        mpsc::Receiver<(JsonValue, SubscriptionSink)>,
+        mpsc::Receiver<(JsonValue, SubscriptionSink)>,
+        mpsc::Receiver<(JsonValue, oneshot::Sender<JsonValue>)>,
+    ) {
+        let mut builder = TestServerBuilder::new();
+
+        let head_rx = builder.register_subscription(
+            "chain_subscribeNewHeads",
+            "chain_newHead",
+            "chain_unsubscribeNewHeads",
+        );
+
+        let finalized_head_rx = builder.register_subscription(
+            "chain_subscribeFinalizedHeads",
+            "chain_finalizedHead",
+            "chain_unsubscribeFinalizedHeads",
+        );
+
+        let block_hash_rx = builder.register_method("chain_getBlockHash");
+
+        let (addr, server) = builder.build().await;
+
+        let client = Client::new(&[format!("ws://{addr}")]).await.unwrap();
+        let api = Api::new(Arc::new(client));
+
+        (api, server, head_rx, finalized_head_rx, block_hash_rx)
+    }
+
+    async fn create_inject_middleware() -> InjectParamsMiddleware {
+        let (api, _server, mut head_rx, _finalized_head_rx, mut block_rx) = create_client().await;
+
+        let (_, head_sink) = head_rx.recv().await.unwrap();
+        head_sink
+            .send(SubscriptionMessage::from_json(&json!({ "number": "0x4321" })).unwrap())
+            .await
+            .unwrap();
+
+        {
+            let (_, tx) = block_rx.recv().await.unwrap();
+            tx.send(json!("0xabcd")).unwrap();
+        }
+
+        InjectParamsMiddleware::new(
+            Arc::new(api),
+            InjectType::BlockHashAt(1),
+            vec![
+                MethodParam {
+                    name: "key".to_string(),
+                    ty: "StorageKey".to_string(),
+                    is_optional: false,
+                    inject: false,
+                },
+                MethodParam {
+                    name: "at".to_string(),
+                    ty: "BlockHash".to_string(),
+                    is_optional: true,
+                    inject: true,
+                },
+            ],
+        )
+    }
+
+    #[tokio::test]
+    async fn skip_inject_if_full_params() {
+        let params = vec![json!("0x1234"), json!("0x5678")];
+        let middleware = create_inject_middleware().await;
+        middleware
+            .call(
+                CallRequest {
+                    method: "state_getStorage".to_string(),
+                    params: params.clone(),
+                },
+                Box::new(move |req: CallRequest| {
+                    async move {
+                        assert_eq!(req.params, params);
+                        Ok(json!("0x1234"))
+                    }
+                    .boxed()
+                }),
+            )
+            .await
+            .unwrap();
+    }
+
+    #[tokio::test]
+    async fn inject_if_without_current_block() {
+        let middleware = create_inject_middleware().await;
+        middleware
+            .call(
+                CallRequest {
+                    method: "state_getStorage".to_string(),
+                    params: vec![json!("0x1234")],
+                },
+                Box::new(move |req: CallRequest| {
+                    async move {
+                        assert_eq!(req.params, vec![json!("0x1234"), json!("0xabcd")]);
+                        Ok(json!("0x1234"))
+                    }
+                    .boxed()
+                }),
+            )
+            .await
+            .unwrap();
+    }
+}
