a5f1a68d45f640af976f59a7ba69353220a6bd6d
Ermal Kaleci <ermalkaleci@gmail.com>
2024-04-10 18:12:12 +0200

    update

--- src/extensions/client/mod.rs
+++ src/extensions/client/mod.rs
@@ -210,10 +210,7 @@ impl Client {
                 // Sort by health score
                 endpoints.sort_by_key(|endpoint| std::cmp::Reverse(endpoint.health().score()));
                 // Pick the first one
-                let selected_endpoint = endpoints[0].clone();
-                // Ensure it's connected
-                selected_endpoint.connected().await;
-                selected_endpoint
+                endpoints[0].clone()
             };
 
             let mut selected_endpoint = healthiest_endpoint(None).await;
